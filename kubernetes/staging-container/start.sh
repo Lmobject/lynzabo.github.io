1a630c737b0b3e858ac38ac6070ccd54984af2ea
lynzabo <lynzabo@hotmail.com>
2018-01-14 22:29:02 +0800

    portal doc

--- kubernetes/staging-container/start.sh
+++ kubernetes/staging-container/start.sh
@@ -4,4 +4,5 @@ set -x
 
 bundle
 bundle exec jekyll clean
-bundle exec jekyll serve -H 0.0.0.0 -P 4000
+#bundle exec jekyll serve -H 0.0.0.0 -P 4000
+bundle exec jekyll serve -H 0.0.0.0 -P 4000 --watch
